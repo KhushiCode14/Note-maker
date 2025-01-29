5ea3f4098da95b2d96527834477670109ed531c8
khushi kumari <khushikumarigupta463@gmail.com>
2025-01-29 21:41:36 +0530

    add User model with schema definition and default values

--- backend/src/Models/UserModel.js
+++ backend/src/Models/UserModel.js
@@ -1,9 +1,38 @@
 import mongoose from "mongoose";
-//Set up default mongoose connection
-var mongoDB = "mongodb://127.0.0.1/my_database";
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+const { Schema } = mongoose; //destructure schema from mongoose
+// console.log(mongoose);
+
+// Define schema
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+});
+
+// Define model
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
