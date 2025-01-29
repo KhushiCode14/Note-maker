d79a3a1cae98760c4b82695ca6d885aed43a1f52
khushi kumari <khushikumarigupta463@gmail.com>
2025-01-29 21:38:47 +0530

    establish MongoDB connection and integrate it into the server setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import app from "./src/app.js";
+import ConnectDB from "./src/config/Db.js";
 
 const PORT = process.env.PORT || 3000;
 
 const createServer = () => {
+  ConnectDB();
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
---
+++ backend/src/config/Db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+//Set up default mongoose connection
+const ConnectDB = () => {
+  var mongoDB = "mongodb://127.0.0.1/my_database";
+  mongoose.connect(mongoDB);
+  //Get the default connection
+  var db = mongoose.connection;
+  //Bind connection to error event (to get notification of connection errors)
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  console.log("mongodb Connected");
+};
+
+export default ConnectDB;
