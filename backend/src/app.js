ab2d70d5f939590219f988d336cd49617250eac6
khushi kumari <khushikumarigupta463@gmail.com>
2025-01-29 22:05:35 +0530

    add routes for user and note APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
+import UserRoutes from "./Routes/UserRoutes.js";
+import NoteRoutes from "./Routes/NoteRoute.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/user", UserRoutes);
+app.use("/api/note", NoteRoutes);
 export default app;
