b9c5293ebec497058ae986789c1d7b0bf7d03668
khushi kumari <khushikumarigupta463@gmail.com>
2025-01-31 12:55:53 +0530

    feat: implement AddNote component with form handling for title and description

--- frontend/src/components/Note/AddNote.jsx
+++ frontend/src/components/Note/AddNote.jsx
@@ -1,7 +1,69 @@
 // import React from "react";
 
+// import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 const AddNote = () => {
-  return <div>Creat note</div>;
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  return (
+    <Box>
+      <FormControl sx={{ m: 1, width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-title">Title</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-title"
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton edge="end"></IconButton>
+            </InputAdornment>
+          }
+          label="Title"
+        />
+      </FormControl>
+      <FormControl sx={{ m: 1, width: "100%" }} variant="outlined">
+        {/* <InputLabel htmlFor="outlined-adornment-description">
+          Description
+        </InputLabel> */}
+        <TextField
+          id="outlined-adornment-description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          multiline
+          rows={4} // You can adjust the number of rows
+          variant="outlined"
+          label="Description"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton edge="end">{/* Add any icon here */}</IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FormControl>
+      <Button>Save Note</Button>
+    </Box>
+  );
 };
 
 export default AddNote;
