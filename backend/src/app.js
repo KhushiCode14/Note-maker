e4f071eecd7f8d07b52b88428e738395475b64da
khushi kumari <khushikumarigupta463@gmail.com>
2025-02-04 16:16:51 +0530

    feat: add CORS middleware and update frontend URL configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,13 +9,13 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [config.frontend_url], // Fixed incorrect URL
+    origin: config.frontend_url, // Fixed incorrect URL
     credentials: true, // Allows cookies & authentication headers
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
 );
-console.log(config.frontend_url);
+console.log(config.frontend_url, config);
 app.use("/api/user", UserRoutes);
 app.use("/api/note", NoteRoutes);
 export default app;
