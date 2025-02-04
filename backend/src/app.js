5ca93107b163082a14aa89528f5bdb830310a683
khushi kumari <khushikumarigupta463@gmail.com>
2025-02-04 16:23:47 +0530

    feat: update CORS configuration to specify frontend URL and enhance logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,13 +9,14 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: config.frontend_url, // Fixed incorrect URL
+    origin: config.frontend_url, // Should be 'https://note-maker-khushicode.netlify.app'
     credentials: true, // Allows cookies & authentication headers
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
 );
-console.log(config.frontend_url);
+app.options("*", cors());
+console.log("frontend url", config.frontend_url);
 app.use("/api/user", UserRoutes);
 app.use("/api/note", NoteRoutes);
 export default app;
